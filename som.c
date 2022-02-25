33a02f4388ce9ceca8d09e062f86f1ed7c9254bf
yassinedam <yassine.dammak@enis.tn>
2022-02-25 12:09:31 +0100

    produit changes commit

--- som.c
+++ som.c
@@ -2,14 +2,15 @@
  
 int main()
 {
-   int a, b, somme;
+   int a, b, somme,prod;
    
    printf("Entrez deux nombres à additionner\n");
    scanf("%d%d", &a, &b);
    
    somme = a + b;
-   
+   prod = a * b ;
    printf("La somme des deux nombres est = %d\n", somme);
+   printf("Le produit des deux nombres est = %d\n", prod);
    
    return 0;
 }
